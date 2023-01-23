3380508c6d54c2f74fbafad9c9e1e893fcd33da2
Chris Young <ctopheryoung@gmail.com>
2023-01-23 13:17:24 -0800

    image carousel

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import Carousel from "./Carousel";
 import fetchPet from "./fetchPet";
 
 const Details = () => {
@@ -9,7 +10,7 @@ const Details = () => {
   if (results.isLoading) {
     return (
       <div className="loading-pane">
-        <h2 className="loader">Loading...</h2>
+        <h2 className="loader">🐶</h2>
       </div>
     );
   }
@@ -18,6 +19,7 @@ const Details = () => {
 
   return (
     <div className="details">
+      <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
         <h2>
